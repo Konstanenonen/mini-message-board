ca1d582da5d44ead1750f13ee449599da496f5f7
Konsta Nenonen <konstanenonen@protonmail.com>
2023-04-16 12:27:46 +0300

    Added messages to the index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hello there!',
+    user: 'Obi-Wan',
+    added: new Date(),
+  },
+  {
+    text: 'Hi mom!',
+    user: 'Konsta',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
